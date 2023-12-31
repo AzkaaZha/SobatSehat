ae40aab08b352bbabdfa2ac0cd6e27fac52a692d
Xmsyd <masayidalfarizqi@gmail.com>
2024-01-01 00:49:58 +0700

    fix dashboard : add card daftar location

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -78,6 +78,25 @@
                             </div>
                         </div>
 
+                        <div class="col-4 mt-4">
+                            <div class="card">
+                                <div class="card-body" style="background-color: #EACBF9;">
+                                    <div class="row  justify-content-center" style="background-color: white; margin-bottom:0.5rem; border-radius: 0.3rem; margin-left: 0; margin-right: 0;">
+                                        <i class="mdi mdi-map fs-5 fc-gradient" style="font-size: 8rem;"></i>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-sm-8">
+                                            <h5 style="font-weight: bold; color: #530986;">Daftar Lokasi</h5>
+                                            <h6 style="font-size: 0.5rem;">3 Daftar Lokasi</h6>
+                                        </div>
+                                        <div class="col-sm-4">
+                                            <a href="" class="btn text-white" style="background-color: #530986; border-radius: 0.2rem;"> <i class="mdi mdi-plus" style="font-weight: bold;"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
                 <!-- Card Section End -->
