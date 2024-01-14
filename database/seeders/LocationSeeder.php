70213094e4d4da7585b74fb2a9660aafe6b784c6
faizabdullah0134 <faizahf0134@gmail.com>
2024-01-14 19:06:28 +0700

    Add database

--- database/seeders/LocationSeeder.php
+++ database/seeders/LocationSeeder.php
@@ -15,17 +15,17 @@ class LocationSeeder extends Seeder
     {
         Location::create([
             'nama_lokasi' => 'Bandung',
-            'gambar' => 'https://e.top4top.io/p_29311frtm1.jpeg',
+            'gambar' => 'https://www.top4top.me/do.php?imgf=top4top_me5ea29de2c1ce0.jpg',
         ]);
 
         Location::create([
             'nama_lokasi' => 'Jakarta',
-            'gambar' => 'https://g.top4top.io/p_2931psklg3.jpeg',
+            'gambar' => 'https://www.top4top.me/do.php?imgf=top4top_me365cae311a830.jpg',
         ]);
 
         Location::create([
-            'nama_lokasi' => 'Depok',
-            'gambar' => 'https://f.top4top.io/p_2931s2kl82.jpeg',
+            'nama_lokasi' => 'Yogyakarta',
+            'gambar' => 'https://www.top4top.me/do.php?imgf=top4top_me363e457032f20.jpg',
         ]);
 
         Location::create([
@@ -36,6 +36,31 @@ class LocationSeeder extends Seeder
         Location::create([
             'nama_lokasi' => 'Solo',
             'gambar' => 'https://f.top4top.io/p_29311rvk71.jpg',
+        ]);
+
+        Location::create([
+            'nama_lokasi' => 'Surabaya',
+            'gambar' => 'https://www.top4top.me/do.php?imgf=top4top_mebd81468e69ba0.jpg',
+        ]);
+
+        Location::create([
+            'nama_lokasi' => 'Semarang',
+            'gambar' => 'https://www.top4top.me/do.php?imgf=top4top_me0f208ccc96360.jpg',
+        ]);
+
+        Location::create([
+            'nama_lokasi' => 'Palembang',
+            'gambar' => 'https://www.top4top.me/do.php?imgf=top4top_meb68f2855855a0.jpg',
+        ]);
+
+        Location::create([
+            'nama_lokasi' => 'Makassar',
+            'gambar' => 'https://www.top4top.me/do.php?imgf=top4top_me11af631e3fd30.jpg',
+        ]);
+
+        Location::create([
+            'nama_lokasi' => 'Surakarta',
+            'gambar' => 'https://www.top4top.me/do.php?imgf=top4top_me9d146d2bf7380.jpg',
         ]);
     }
 }
