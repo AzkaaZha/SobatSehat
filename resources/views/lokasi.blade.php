2749a587b76b4db170300440abc5fa09633e059a
faizabdullah0134 <faizahf0134@gmail.com>
2024-01-15 23:19:16 +0700

    Update database

--- resources/views/lokasi.blade.php
+++ resources/views/lokasi.blade.php
@@ -22,7 +22,7 @@
 </div>
 <nav class="navbar navbar-expand-lg navbar-light shadow-sm" style="background-color: #D2DBF8;">
     <div class="container">
-        <a class="navbar-brand" href="{{ url('/home')}}"><img src="{{ asset('page/assets/logoSS.png') }}" alt="Logo" style="height: 3.6rem;"></a>
+        <a class="navbar-brand" href="#"><img src="{{ asset('page/assets/logoSS.png') }}" alt="Logo" style="height: 3.6rem;"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
